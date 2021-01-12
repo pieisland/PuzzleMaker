e73d5ae7871188721dbd25c4d9c379eb458715b2
pieisland <pieisland@naver.com>
2021-01-12 18:09:26 +0900

    feat : PhotoFrame - 불러온 이미지 파일을 height 400px로 고정한 상태로 표시 - 미리 만들어진 grid를 이미지 위에 표시

---
+++ src/components/PhotoFrame.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+const PhotoWrapper = styled.div`
+  //height: 30%;
+  position: relative;
+`;
+
+const PhotoImg = styled.img`
+  //max-height: 100%;
+  //width: auto;
+  height: 400px;
+  position: absolute;
+`;
+
+const PhotoGrid = styled.img`
+  height: 400px;
+  position: absolute;
+`;
+
+const PhotoFrame = () => {
+  return (
+    <>
+      <PhotoWrapper id="photoWrapper">
+        <PhotoImg id="photoImg"></PhotoImg>
+        <PhotoGrid id="photoGrid"></PhotoGrid>
+      </PhotoWrapper>
+    </>
+  );
+};
+
+export default PhotoFrame;
